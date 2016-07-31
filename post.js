9235297f4c1beaaf0ac166bc5cee80513d861d19
FrogAhn <rmfja5683@naver.com>
2016-07-31 18:28:54 +0900

    db에 게시글 저장용

--- post.js
+++ post.js
@@ -8,5 +8,5 @@ var db = require('./db.js'),
         body:       {type: String, required: true},
         date:       {type: Date, required: true, default: Date.now()}
     });
-
+ 
 module.exports= Post;
