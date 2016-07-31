6b32ded58271213936893c7a8b4917325834e5ff
HongBum Choi <HongBum Choi>
2016-07-31 15:49:39 +0900

    UI improved. 0731

--- public/javascripts/bannerCtrl.js
+++ public/javascripts/bannerCtrl.js
@@ -13,7 +13,7 @@ app.controller('bannerCtrl', function ($scope) {
     var banner2 = {};
 
     banner2.pfPic = "https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/11885156_728851200554207_4828033296060397603_n.jpg?oh=9ac0ea1a2ab6f125f11c83a42cb516e1&oe=5825FFA0";
-    banner2.body = " It's a nice day!";
+    banner2.body = " It's a nice day testttttttttttttttttttttttttttttttttttttttttttttttttt!";
     banner2.username = "handsome_guy  ";
     banner2.date = "15:11";
 
