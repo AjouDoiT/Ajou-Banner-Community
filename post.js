73a6782c0533eda7e296b092cad9b68babfa8082
FrogAhn <rmfja5683@naver.com>
2016-07-31 17:54:57 +0900

    map & post db

---
+++ post.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+var db = require('./db.js'),
+    Post = db.model('Post', {
+    	_id:	{type: String, required: true},
+        username:   {type: String, required: true},
+        body:       {type: String, required: true},
+        date:       {type: Date, required: true, default: Date.now()}
+    });
+
+module.exports= Post;
