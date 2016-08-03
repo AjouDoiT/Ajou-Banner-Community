f32adbed5ec152c53127d863433853a3d70faf62
Lawrence Kay <lkay9495@hotmail.com>
2016-08-03 16:09:10 +0900

    Extra commit for unresolved conflict

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-  res.sendFile(path.resolve('views/index.html'));
-=======
     res.sendFile(path.resolve('views/index.html'));
->>>>>>> 0a1abee8d98804742a40ae3b652ad5efe73a51d9
 });
 
 module.exports = router;
