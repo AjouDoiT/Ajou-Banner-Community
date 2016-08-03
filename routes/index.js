4ca2d1cc45c8ea4e2525386853c847f0e7714129
Lawrence Kay <lkay9495@hotmail.com>
2016-08-03 17:23:24 +0900

    Working on JWT Authentication. See JWT Authentication Progress.md for specific progress.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var path = require('path');
+var firebase = require('firebase');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.resolve('views/index.html'));
+  res.sendFile(path.resolve('views/custom_sample.html'));
+});
+
+// Receiving JWT Token through POST Method
+router.post('/data', function(req, res,next){
+	res.send('Login Successful');
+	console.log(res.json(req.body));
 });
 
 module.exports = router;
