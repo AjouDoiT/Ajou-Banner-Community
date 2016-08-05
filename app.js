13dccf0c1477b08ba9e67fcffa98b15ef55d02be
FrogAhn <rmfja5683@naver.com>
2016-08-05 18:07:39 +0900

    app.set 엔진부분 코드 추가

--- app.js
+++ app.js
@@ -7,15 +7,19 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var app = express();	
 
+
 var mongoose = require('mongoose');
 var Post = require('./model/post');
 var dbroutes = require('./routes/db')(app,Post);
 
 
-
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+/**
+ * we will (temporally) use raw html file by sendfile method .
+ * by credtiger96
+ */
 
 // we have to make favicon
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -54,23 +58,18 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.sendFile(path.resolve('views/error.html'));
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.sendFile(path.resolve('views/error.html'));
 });
+
 
 module.exports = app;
