10523ab64f66ec4282de61f287ae3a8bae536db6
FrogAhn <rmfja5683@naver.com>
2016-08-05 17:33:39 +0900

    수정한건 없고 띄어쓰기 없앰

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
-var app = express();
+var app = express();	
 
 var mongoose = require('mongoose');
 var Post = require('./model/post');
