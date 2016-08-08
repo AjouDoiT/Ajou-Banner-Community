6bee84888ffee8d366ea8df527f5e01ae82128e3
myeongjin1220 <myeongjin1220@gamil.com>
2016-08-08 22:44:13 +0900

    add to favicon

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // we have to make favicon
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
